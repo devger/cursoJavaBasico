be1e1515654176e21bb09fd185ddd8c1e11d4d7c
devger <devger.ritchie@gmail.com>
2015-07-03 21:22:00 -0300

    nuevos archivos agregados

---
+++ bloque4.3/src/bloque43.java
@@ -0,0 +1,30 @@
+
+public class bloque43 {
+
+	static public void main (String[] arg) {
+		
+		int[] vector = {2,3,4,2,32434,34,4,4,3,233,2,321,1,0};
+		int temporal;
+		
+		for(int i=0; i<vector.length; i++) {
+			System.out.print(vector[i] +" ");
+		}
+		System.out.println("");
+		
+		for(int i=0; i<vector.length; i++) {
+			for(int j=i+1; j<vector.length; j++) {
+				if(vector[i]<vector[j]){
+					temporal = vector[i];
+					vector[i] = vector[j];
+					vector[j] = temporal;
+				}
+			}
+		}
+		
+		for(int i=0; i<vector.length; i++) {
+			System.out.print(vector[i] +" ");
+		}
+		System.out.println("");
+	}
+	
+}
